8cc6fde13423bbcbf7c6c5de0b1bc168c16758bd
Jonas Högne <hogne@student.chalmers.se>
2020-12-01 15:36:13 +0100

    Day 1 done

--- 2020/01/day01.py
+++ 2020/01/day01.py
@@ -1,2 +1,11 @@
 with open('input.txt', 'r') as f:
     input = f.read().splitlines()
+
+values = {int(i) : True for i in input}
+
+for v in values.keys():
+    if(values.get(2020 - v)):
+        print("Part 1: {}".format(v * (2020 - v)))
+    for v2 in values.keys() :
+        if(values.get(2020 - v - v2)):
+            print("Part 2: {}".format(v * v2 * (2020 - v -v2)))
