8837bf382f8271e4e886e92c170fa41fd49b23fd
Jhogne <hogne@student.chalmers.se>
2020-12-07 10:42:09 +0100

    Refactoring day 7

--- 2020/07/day07.py
+++ 2020/07/day07.py
@@ -4,29 +4,25 @@ import re
 with open('input.txt', 'r') as f:
     lines = f.read().splitlines()
 
-rules = defaultdict(list)
+contains = defaultdict(list)
+parent = defaultdict(list)
 for line in lines:
     outer, inner = re.split(' bags contain ', line)
-    for numbag in inner.split(', '):
-        if(numbag == 'no other bags.'):
-            break
-        rules[outer].append((int(numbag[0]), numbag.rsplit(' ',1)[0][2:]))
-        
+    for amt_color in inner.split(', '):
+        if(amt_color != 'no other bags.'):
+            color = amt_color.rsplit(' ',1)[0][2:]
+            contains[outer].append((int(amt_color[0]), color))
+            parent[color].append(outer)
+
 def holds_bag(target):
     bags = set()
-    outer = [bag for bag in rules.keys() if target in [tup[1] for tup in rules[bag]]]
-    for bag in outer:
+    for bag in parent[target]:
         bags.add(bag)
         bags.update(holds_bag(bag))
     return bags
 
-def bags_contained(target):
-    if not rules[target]:
-        return 0
-    tot = 0
-    for num,color in rules[target]:
-        tot += num + num * bags_contained(color)
-    return tot
+def bags_contained_in(target):
+    return sum(num + num * bags_contained_in(color) for num,color in contains[target])
 
 print("Part 1:",len(holds_bag('shiny gold')))
-print("Part 2:",bags_contained('shiny gold'))
+print("Part 2:",bags_contained_in('shiny gold'))
