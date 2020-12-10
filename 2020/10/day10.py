cd772e4dc60a2299fcbc6c28f7cb6612141ec63f
Jhogne <hogne@student.chalmers.se>
2020-12-11 00:57:50 +0100

    Final refactor day 10

--- 2020/10/day10.py
+++ 2020/10/day10.py
@@ -6,54 +6,41 @@ jolts.sort()
 jolts.append(jolts[-1]+3)
 
 # Part 1
-jolt1 = 0
-jolt3 = 0
+diff1 = 0
+diff3 = 0
 last = 0
 for i in jolts:
     if(i - last == 1):
-        jolt1 += 1
+        diff1 += 1
     elif(i - last == 3):
-        jolt3 += 1
+        diff3 += 1
     last = i
 
 # Part 2
-# Count the number of ways to remove adapters from the full sequence 
-# No more than two subsequent adapters can be removed at the same time, minimal example:
-# 1 2 3 4 5
-# if 2 3 4 are removed causes the difference to be too large (5 - 1 = 4; 4 > 3)
+# Construct the sequence from left to right. Each optional doubles the number of 
+# possible sequences, except if the two predecessors are optional, because a maximum
+# of three optional can be excluded at the same time:
+# Minimal example: 1 [2 3 4] 5 => 5 - 1 = 4; 4 > 3
+# When the two predecessors are optional the third is no longer optional, therefore the number 
+# of cases increases by 7/4 (double - 1/4):
+# x... prev1 prev2 _/curr  > (2 cases)
+# x... prev1 _     _/curr  > (2 cases)
+# x... _     prev2 _/curr  > (2 cases)
+# x... _     _       curr  > (1 case)
 
-# 1. Find all removable adapters
-chunks = [jolts[i:i+3] for i in range(len(jolts)-2)]
-removable = []
-for a,b,c in chunks:
+optional = []
+for a,b,c in [jolts[i:i+3] for i in range(len(jolts)-2)]:
     if c - a <= 3:
-        removable.append((a,b,c))
+        optional.append((a,b,c))
 
-# 2. Find adapters that can be removed when the previous removable adapter is removed
-removable_without_prev = []
-new_pre = removable[0][0]
-for pre,curr,post in removable[1:]:
-    if post - new_pre <= 3:
-        removable_without_prev.append(curr)
-    new_pre = pre
+count = 2 # First optional always causes two cases (Choose it or not)
+pre_pre_is_optional = False
+for (pre,_,post),prev_optional in zip(optional[1:], [x[1] for x in optional]):
+    if pre == prev_optional and pre_pre_is_optional: 
+        count *= 7 / 4 
+    else:
+        count *= 2 
+    pre_pre_is_optional = post - prev_optional <= 3
 
-# 3. Count the number of ways to remove adapters and still have a valid sequence
-count = 2 # First removable always causes two cases (Choose it or not)
-two_removable_before = False
-# Iterate over all removable adapters, aswell as the removable adapter to it's left
-for (pre,curr,_),prev_rem in zip(removable[1:], [x[1] for x in removable]):
-    # If curr is removable, and the two predecessors are also removable.
-    if pre == prev_rem and two_removable_before: 
-        # Double, but without the quarter of the cases where previous two are removed 
-        # prev1 prev2 < ok
-        # _     prev2 < ok
-        # prev1 _     < ok
-        # _     _     < not ok since max 2 subsequent adapters can be removed
-        count += 3 * count / 4 
-    # Otherwise, if predecessor isn't removable, or only the immidiate predecessor is removable
-    elif pre != prev_rem or curr in removable_without_prev:
-        count *= 2 # The current adapter is either added to the sequence, or not, doubling the cases
-    two_removable_before = curr in removable_without_prev
-
-print("Part 1:",jolt1*jolt3)
+print("Part 1:",diff1*diff3)
 print("Part 2:",int(count))
