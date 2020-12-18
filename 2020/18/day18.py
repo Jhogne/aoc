a05df2e1707a990f02e23a9121dff545286d9e69
Jhogne <hogne@student.chalmers.se>
2020-12-18 19:45:32 +0100

    Refactor day 18

--- 2020/18/day18.py
+++ 2020/18/day18.py
@@ -1,53 +1,46 @@
-from itertools import product
-import pyparsing
+import re
 
 with open('input.txt', 'r') as f:
     lines = f.read().splitlines()
 
-def get_next(expr):
-    #print('Get next',expr)
+def left_precedence(expr):
+    if expr[1].isdigit():
+        return expr[2:], int(expr[1])
+    elif expr[1] == '(':
+        return eval(expr[2:], left_precedence)
+
+def eval_add(expr):
+    _, tot, expr = re.split(r'(\d+)',expr, maxsplit=1)
+    tot = int(tot)
+    if len(expr) > 0 and expr[0] == '+':
+        expr, inc = plus_precedence(expr)
+        tot += inc
+    return expr, tot
+
+def plus_precedence(expr):
     if expr[1].isdigit():
         return eval_add(expr[1:])
     elif expr[1] == '(':
-        new_expr, tot = eval(expr[2:])
-        return eval_add(str(tot)+new_expr)
+        new_expr, tot = eval(expr[2:], plus_precedence)
+        return eval_add(str(tot) + new_expr)
 
-# 1 + 2 * 3 + 4 * 5 + 6
-def eval_add(expr):
-    start = 0
-    for c in expr:
-        if c.isdigit():
-            start += 1
-        else:
-            break
-    tot = int(expr[0:start])
-    expr = expr[start:]
-    while len(expr) > 0 and expr[0] == '+':
-        expr,inc = get_next(expr)
-        tot += inc
-    return expr,tot
-
-def eval(expr):
-    expr,tot = get_next('0'+expr)
+def eval(expr, right_term):
+    expr, tot = right_term(' ' + expr)
     while len(expr) > 0:
-        #print(expr,tot)
         if expr[0] == '+':
-            expr, inc = get_next(expr)
+            expr, inc = right_term(expr)
             tot += inc
         elif expr[0] == '*':
-            expr, inc = get_next(expr)
+            expr, inc = right_term(expr)
             tot *= inc
         elif expr[0] == ')':
-            return expr[1:],tot
-    return '',tot
+            return expr[1:], tot
+    return tot
 
-
-#print(eval("(1+2)*(2+3)+1")) 
-tot = 0
+p1 = p2 = 0
 for line in lines:
-    tot += eval(line.replace(' ',''))[1]
+    p1 += eval(line.replace(' ', ''), left_precedence)
+    p2 += eval(line.replace(' ', ''), plus_precedence)
 
-
-
-print("Part 1:",tot)
-print("Part 2:")
+print("Part 1:", p1)
+print("Part 2:", p2)
