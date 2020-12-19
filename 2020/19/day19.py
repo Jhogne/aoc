58e3fca71b1f48d49f61d549008356e873555a21
Jhogne <hogne@student.chalmers.se>
2020-12-19 18:17:09 +0100

    Refactor day 19

--- 2020/19/day19.py
+++ 2020/19/day19.py
@@ -1,42 +1,24 @@
 import regex
-import time
 
-start_time = time.time()
 with open('input.txt', 'r') as f:
-    first, msgs = f.read().strip().split('\n\n')
+    rules, msgs = f.read().strip().split('\n\n')
 
-rules1 = {}
-start = {}
-for rule in first.split('\n'):
-    idx, pattern = rule.split(': ')
-    pattern = " " + pattern.replace("\"","") + " "
-    rules1[idx] = pattern
-    if pattern == ' a ' or pattern == ' b ':
-        start[idx] = pattern
+rules1 = dict([rule.split(': ') for rule in rules.splitlines()])
 
 rules2 = rules1.copy()
 rules2['8'] = "(?<eight> 42 | 42 (?&eight) )"
 rules2['11'] = "(?<eleven> 42 31 | 42 (?&eleven) 31 )"
 
+def rule_to_regex(num, rules):
+    if num in (")", "(?<eight>", "(?&eight)", "(?<eleven>", "(?&eleven)") or num.startswith("\""):
+        return num.replace('\"', "")
+    terms = rules[num].split(" | ")
+    # Call rule_to_regex for each value in term and put in a string
+    res = ["".join(rule_to_regex(v, rules) for v in vals.split(" "))
+           for vals in terms]
+    return "(" + "|".join(res) + ")"
 
-def solve(rules, to_replace):
-    keep_updating = True
-    while keep_updating:
-        new = {}
-        for rplc in to_replace:
-            for rule in rules:
-                updated = regex.sub(
-                    f" {rplc} ", f" {to_replace[rplc]} ", rules[rule])
-                rules[rule] = updated
-                if not regex.search(r"\d", updated):
-                    new[rule] = "(" + updated + ")"
-        to_replace, keep_updating = new, len(new) != len(rules)
-
-    return len([msg for msg in msgs.split('\n')
-                if regex.fullmatch(rules['0'].replace(" ", ""), msg) != None])
-
-
-print("Part 1:", solve(rules1, start))
-print("Part 2:", solve(rules2, start))
-
-print(time.time() - start_time)
+p1 = regex.compile(rule_to_regex('0', rules1))
+p2 = regex.compile(rule_to_regex('0', rules2))
+print("Part 1:", sum([bool(p1.fullmatch(msg)) for msg in msgs.split('\n')]))
+print("Part 2:", sum([bool(p2.fullmatch(msg)) for msg in msgs.split('\n')]))
