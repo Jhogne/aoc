b0752c3d68202814daecbe4517def36890b5b970
Jhogne <hogne@student.chalmers.se>
2020-12-21 21:19:12 +0100

    Refactor day 21

--- 2020/21/day21.py
+++ 2020/21/day21.py
@@ -1,47 +1,24 @@
-from collections import defaultdict
-
 with open('input.txt', 'r') as f:
     lines = f.read().splitlines()
 
-
-atof = defaultdict(set)
-all_items = set()
+atof, ingredients = {}, []
 for l in lines:
     items, allergens = l.split(' (contains ')
-    allergens = allergens[:-1]
     items = items.split(" ")
-    all_items.update(items)
-    allergens = allergens.split(", ")
-    for a in allergens:
-        if len(atof[a]) == 0:
+    ingredients += items
+
+    for a in allergens[:-1].split(", "):
+        if atof.get(a) == None:
             atof[a] = set(items)
         else:
-            candidates = set(items) & atof[a]
-            atof[a] = candidates
-            if len(candidates) == 1:
-                for k in atof:
-                    if k != a:
-                        atof[k] = atof[k] - candidates
+            atof[a]  = set(items) & atof[a]
+            if len(atof[a] ) == 1:
+                atof.update(
+                    {k: atof[k] - atof[a]  for k in atof.keys() if k != a})
 
-for k in atof:
-    all_items -= atof[k]
-print(atof)
-print(all_items)
-count = 0
-for l in lines:
-    items, _ = l.split(' (contains ')
-    for item in items.split(' '):
-        if item in all_items:
-            count += 1
+no_allergens = set(ingredients).difference(*[atof[k] for k in atof])
+p1 = sum([item in no_allergens for item in ingredients])
+p2 = ','.join(atof[k].pop() for k in sorted(atof.keys()))
 
-
-keys = atof.keys()
-keys = sorted(keys)
-print(keys)
-p2 = ''
-for key in keys:
-    p2 += atof[key].pop()
-    p2 += ','
-
-print("Part 1:", count)
-print("Part 2:", p2[:-1])
+print("Part 1:", p1)
+print("Part 2:", p2)
