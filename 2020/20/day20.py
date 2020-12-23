0920880eb7c983204e74ae2a8c90e2a113b9a49b
Jhogne <hogne@student.chalmers.se>
2020-12-23 01:18:21 +0100

    Refactor/rewrite day 20

--- 2020/20/day20.py
+++ 2020/20/day20.py
@@ -1,280 +1,154 @@
-from collections import defaultdict
-import math
-import time
+from math import sqrt
 
-start_time = time.time()
 with open('input.txt', 'r') as f:
-    tiles = f.read().strip().split('\n\n')
+    data = f.read().strip().split('\n\n')
 
+# Get the border index of adjacent tile in direction y,x
+adjacent_to_edge = {(1, 0): 0, (0, -1): 1, (-1, 0): 2, (0, 1): 3}
+# Gets the adjacent tile y,x facing the border with given index
+edge_to_adjacent = {0: (-1, 0), 1: (0, 1), 2: (1, 0), 3: (0, -1)}
 
-edges = {}
-pixels = {}
-for tile in tiles:
-    rows = tile.split('\n')
-    id, data = rows[0][5:9], rows[1:]
-    top, bot = data[0], data[-1]
-    left = "".join([row[0] for row in data])
-    right = "".join([row[-1] for row in data])
-    edges[int(id)] = {"W": left, "E": right, "N": top, "S": bot}
-    pixels[int(id)] = data
+SIZE = int(sqrt(len(data)))
+MONSTER = ['                  # ',
+           '#    ##    ##    ###',
+           ' #  #  #  #  #  #   ']
+tiles = {}
+for d in data:
+    rows = d.split('\n')
+    id, tile = rows[0][5:9], rows[1:]
+    tiles[int(id)] = tile
 
+def get_borders(tile):
+    top, bot = tile[0], tile[-1]
+    left = "".join([row[0] for row in tile])
+    right = "".join([row[-1] for row in tile])
+    return [top, right, bot, left]
 
-def edge_comp(edge1, edge2):
-    if edge1 == edge2:
-        return True
-    elif edge1[::-1] == edge2:
-        return True
-    else:
+def get_matches(tile, tiles):
+    # Compare the edges of all tiles with all possible edges for given tile.
+    # Returns list of ids (or None) of adjacent tiles. Order is [North, East, South, West]
+
+    matches = [None] * 4
+    my_boarders = get_borders(tile)
+    my_boarders += [b[::-1] for b in my_boarders]
+
+    for other in tiles:
+        if tiles[other] == tile:
+            continue
+
+        other_borders = get_borders(tiles[other])
+
+        for i, b in enumerate(my_boarders):
+            if b in other_borders:
+                matches[i % 4] = other
+
+    return matches
+
+def rotate(tile):
+    return [''.join(ele) for ele in list(
+        map(list, list(zip(*tile[::-1]))))]
+
+def flip(tile):
+    new_tile = tile.copy()
+    for j, row in enumerate(tile):
+        new_tile[j] = ''.join(reversed(row))
+    return new_tile
+
+def transform_tile(tile, board, y, x):
+    other_tile, other_edge = [], -1
+
+    # Find adjacent (correctly transformed) tile in the board. Use it's border for reference to
+    # transform current tile properly.
+    for dy, dx in adjacent_to_edge:
+        if 0 <= y + dy < SIZE and 0 <= x + dx < SIZE and board[y+dy][x+dx] != None:
+            other_tile = board[y+dy][x+dx]
+            other_edge = adjacent_to_edge[(dy, dx)]
+
+    assert other_tile != [] and other_edge != -1
+    other_boarder = get_borders(other_tile)[other_edge]
+    for _ in range(4):
+        for _ in range(2):
+            my_boarder = get_borders(tiles[tile])[(other_edge + 2) % 4]
+            if my_boarder == other_boarder:
+                return
+            tiles[tile] = flip(tiles[tile])
+        tiles[tile] = rotate(tiles[tile])
+
+def assemble_board(start):
+    # Initiat with start at the top left of the board:
+    # 1. Get next tile
+    # 2. Transform it if it isn't the first (as it needs to be used for reference)
+    # 3. Add (transformed tile) to board
+    # 4. Add all adjacent tiles to queue if they aren't already placed or are waiting to be placed
+
+    q = [(start, 0, 0)]
+    board = [[None for _ in range(SIZE)] for _ in range(SIZE)]
+
+    while len(q) > 0:
+        curr, y, x = q.pop(0)
+        if not (x == 0 and y == 0):
+            transform_tile(curr, board, y, x)
+
+        board[y][x] = tiles[curr]
+        for i, neighbour in enumerate(get_matches(tiles[curr], tiles)):
+            if neighbour == None:
+                continue
+            dy, dx = edge_to_adjacent[i]
+            if board[y+dy][x+dx] == None and (neighbour, y+dy, x+dx) not in q:
+                q.append((neighbour, y+dy, x+dx))
+    return board
+
+def remove_borders(board):
+    # Removes the boarders of each tile in the board
+    out = []
+    for row in board:
+        for y in range(1, len(row[0])-1):
+            line = ''
+            for col in row:
+                for x in range(1, len(col)-1):
+                    line += col[y][x]
+            out.append(line)
+    return out
+
+def is_monster(x, y, image):
+    # Checks if position x,y in image is the top left pixel of a monster
+    if y + len(MONSTER) > len(image) or x + len(MONSTER[0]) > len(image[0]):
         return False
+    for dy, row in enumerate(MONSTER):
+        for dx, cell in enumerate(row):
+            if cell == '#' and image[y+dy][x+dx] != '#':
+                return False
+    return True
+
+def count_monsters(image):
+    # Counts the monsters in each flip/rotation combination of the image. Assuming all monsters are
+    # turned the same way => this will return the number of monsters in the single orientation
+    count = 0
+    for _ in range(4):
+        for _ in range(2):
+            for y in range(len(image)):
+                for x in range(len(image)):
+                    if(is_monster(x, y, image)):
+                        count += 1
+            image = flip(image)
+        image = rotate(image)
+    return count
 
 
-def rotate_cell(edges, id):
-    olds = edges[id].copy()
+part1 = 1
+start = -1
+for id, tile in tiles.items():
+    matches = get_matches(tile, tiles)
+    part1 *= id if sum([x != None for x in matches]) == 2 else 1
 
-    for edge in olds:
-        if edge == "W":
-            new = olds.get("S")
-        elif edge == "N":
-            new = olds.get("W")
-            if new != None and type(new) != int:
-                new = new[::-1]
-        elif edge == "E":
-            new = olds.get("N")
-        elif edge == "S":
-            new = olds.get("E")
-            if new != None and type(new) != int:
-                new = new[::-1]
-
-        edges[id][edge] = new
-
-
-def flip_cell(edges, id, yaxis):
-    olds = edges[id].copy()
-
-    for edge in olds:
-        if yaxis:
-            if edge == "W" or edge == "E":
-                new = olds.get(flip_edge(edge))
-            else:
-                new = olds.get(edge)
-                if new != None and type(new) != int:
-                    new = new[::-1]
-        else:
-            if edge == "N" or edge == "S":
-                new = olds.get(flip_edge(edge))
-            else:
-                new = olds.get(edge)
-                if new != None and type(new) != int:
-                    new = new[::-1]
-        edges[id][edge] = new
-
-
-def flip_edge(edge):
-    if edge == "W":
-        return "E"
-    elif edge == "N":
-        return "S"
-    elif edge == "E":
-        return "W"
-    elif edge == "S":
-        return "N"
-
-
-matches = defaultdict(dict)
-start = 0
-for id in edges:
-    for edge in edges[id]:
-        for other_id in edges:
-            for edge2 in edges[other_id]:
-                same = edge_comp(edges[id][edge], edges[other_id][edge2])
-                if id != other_id and same:
-                    matches[id][edge] = other_id
-                else:
-                    if not matches[id].get(edge):
-                        matches[id][edge] = None
-    if matches[id].get('S') != None and matches[id].get('N') == None and matches[id].get('W') == None and matches[id].get('E') != None:
+    # Start with an arbitrary tile that can fit in top left spot without transforming it.
+    if matches[0] == None and matches[1] != None and matches[2] != None and matches[3] == None:
         start = id
+assert start != -1
 
-
-def exact_edge_comp(edge1, edge2):
-    if edge1 == edge2:
-        return 2
-    elif edge1[::-1] == edge2:
-        return 1
-    else:
-        return 0
-
-
-def replace_char(orig, i, char):
-    return orig[0:i] + char + orig[i+1:]
-
-
-def flipx():
-    flip_cell(edges, curr, True)
-    flip_cell(matches, curr, True)
-    for i in range(0, (len(pixels[curr])//2)):
-        for j, row in enumerate(pixels[curr]):
-            save = row[len(pixels[curr]) - 1 - i]
-            row = replace_char(
-                row, len(row) - 1 - i, row[i])
-            row = replace_char(row, i, save)
-            pixels[curr][j] = row
-
-
-def flipy():
-    flip_cell(edges, curr, False)
-    flip_cell(matches, curr, False)
-
-    for i in range((len(pixels[curr])//2)):
-        pixels[curr][i], pixels[curr][len(
-            pixels[curr]) - 1 - i] = pixels[curr][len(pixels[curr]) - 1 - i], pixels[curr][i]
-
-
-def flip(e1, e2, edge):
-    comp = exact_edge_comp(e1, e2)
-    if comp == 1:
-        if edge == "N" or edge == "S":
-            flipx()
-        elif edge == "E" or edge == "W":
-            flipy()
-        return edges[curr][flip_edge(edge)]
-    elif comp == 0:
-        if exact_edge_comp(edges[curr][edge], e2) != 0:
-            if edge == "N" or edge == "S":
-                flipy()
-            else:
-                flipx()
-            if exact_edge_comp(edges[curr][edge], e2) == 1:
-                if edge == "N" or edge == "S":
-                    flipx()
-                else:
-                    flipy()
-        return edges[curr][flip_edge(edge)]
-
-
-def rotate():
-    rotate_cell(edges, curr)
-    rotate_cell(matches, curr)
-    pixels[curr] = [''.join(ele) for ele in list(
-        map(list, list(zip(*pixels[curr][::-1]))))]
-
-
-def transform_cell(curr, y, x, done, matches, edges):
-    for cell in done:
-        for edge in done[cell]:
-            if done[cell][edge] == curr:
-
-                e1 = edges[curr][flip_edge(edge)]
-                e2 = edges[cell][edge]
-
-                comp = exact_edge_comp(e1, e2)
-                while comp != 2:
-                    e1 = flip(e1, e2, edge)
-                    while exact_edge_comp(e1, e2) == 0:
-                        rotate()
-                        e1 = edges[curr][flip_edge(edge)]
-                    comp = exact_edge_comp(e1, e2)
-
-
-def move(x, y, dir):
-    if edge == "N":
-        return x, y - 1
-    elif edge == "S":
-        return x, y + 1
-    elif edge == "E":
-        return x + 1, y
-    elif edge == "W":
-        return x - 1, y
-
-
-size = int(math.sqrt(len(edges)))
-img = [['' for _ in range(size)] for _ in range(size)]
-
-done = {}
-pos = {}
-q = []
-q.append(start)
-
-while len(q) > 0:
-    x = y = 0
-    curr = q.pop()
-    for cell in done:
-        for edge in done[cell]:
-            if done[cell][edge] == curr:
-                x, y = move(*pos[cell], edge)
-
-    transform_cell(curr, y, x, done, matches, edges)
-    done[curr] = matches[curr]
-    pos[curr] = (x, y)
-
-    if img[y][x] == '':
-        img[y][x] = pixels[curr]
-
-    for edge in done[curr]:
-        if done[curr][edge] != None and done[curr][edge] not in done:
-            q.append(matches[curr][edge])
-
-
-monster = ['..................#.','#....##....##....###','.#..#..#..#..#..#...']
-monsters = [[0]]*8
-for i in range(len(monsters)):
-    if i == 4:
-        new = [0]*len(monster)
-        for j, row in enumerate(monster):
-            new[j] = row[::-1]
-        monster = new
-
-    monsters[i] = monster
-    monster = list(zip(*monster[::-1]))
-
-
-new_img = [['.' for _ in range(size)] for _ in range(size)]
-for x, row in enumerate(img):
-    for y, cell in enumerate(row):
-        new = [['.' for _ in range(len(cell)-2)] for _ in range(len(cell)-2)]
-
-        for i in range(1, len(cell[0])-1):
-            for j in range(1, len(cell[0])-1):
-                new[i-1][j-1] = cell[i][j]
-        for i, row in enumerate(new):
-            new[i] = ''.join(row)
-
-        new_img[x][y] = new
-
-
-final = ['.' for _ in range(len(new_img[0][0])*size)]
-offset = 0
-for row in new_img:
-    for i in range(len(row[0])):
-        r = []
-        for cell in row:
-            r.append(cell[i])
-        final[offset+i] = ''.join(r)
-    offset += len(new_img[0][0])
-
-count = 0
-
-final = [''.join(s) for s in list(zip(*final[::-1]))]
-for i, row in enumerate(final):
-    for j, col in enumerate(row):
-        for mon in monsters:
-            found = True
-            candidate = []
-            for y in range(len(mon)):
-                for x in range(len(mon[y])):
-                    if mon[y][x] == '#':
-                        if 0 <= i+y < len(final) and 0 <= j+x < len(final[0]) and final[i+y][j+x] == '#':
-                            found = True
-                            candidate.append((i+y, j+x))
-                        else:
-                            found = False
-                            break
-                if not found:
-                    break
-            if found:
-                count += 1
-
-print(count)
-print(sum(s.count('#') for s in final) - (count * 15))
-
-print(time.time() - start_time)
+board = assemble_board(start)
+image = remove_borders(board)
+part2 = sum(row.count('#') for row in image) - \
+    (count_monsters(image) * sum(row.count('#') for row in MONSTER))
+print("Part 1:", part1)
+print("Part 2:", part2)
