ad80d347ba2318b2638d3e19dd7e904bb2540a65
Jonas Högne <hogne@student.chalmers.se>
2024-12-14 07:22:48 +0100

    Use cache instead of lru_cache

--- 2024/.template.py
+++ 2024/.template.py
@@ -1,6 +1,6 @@
 import re
 import math
-from functools import lru_cache
+from functools import cache
 from ast import literal_eval
 from itertools import count, pairwise
 from collections import defaultdict
